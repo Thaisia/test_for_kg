ca4d3ecf3fef3cc491df5e03cd59e5c975f2002e
Taisia Bo <thaisbox@gmail.com>
2019-07-30 22:31:41 +0300

    styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'items#index'
-  get 'import' => 'imports#import'
+  root 'items#index', as: 'home'
+  get 'import' => 'imports#import', as: 'import'
 
   resources :items
 
